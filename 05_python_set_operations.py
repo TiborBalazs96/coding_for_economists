357ede49f5fd3c05ddf08aac50c5757ec0a780fb
TiborBalazs96 <balazstibi16@gmail.com>
2024-01-18 14:02:50 +0000

    Added script for set operations

---
+++ 05_python_set_operations.py
@@ -0,0 +1,38 @@
+#Intro to data type set
+
+mySet={1,2,3}
+print(mySet)
+
+print(type(mySet))
+
+#Sets only contain uniqe values
+mySet={1,2,3,3,3,3,3,3,3,3,3}
+
+print(mySet)
+
+#Duplicates are removed
+
+#Check membership
+
+print(1 in mySet)
+
+print(set('aaaaaaaaabbbbbbbccccccc'))
+
+#set is unordered collection of elements
+
+#Define two sets and check out set operations
+
+setA = {1,2,3}
+listB={3,4,5,5,5}
+setB=set(listB)
+
+print(setA,setB)
+
+#set union
+
+print(setA|setB)
+
+
+
+
+
