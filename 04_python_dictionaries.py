e2030ba5362ae5e96400dc3baee3a55529e9cade
Tibor Balazs <balazstibi16@gmail.com>
2024-01-18 17:25:10 +0000

    Added script for python dictionaries

---
+++ 04_python_dictionaries.py
@@ -0,0 +1,17 @@
+#First look at dictionaries
+
+#A dictionary is a collection of key-value pairs
+
+myDict = {'Dog': 'Barks', 'Cat': 'Meows', 'Whale': 'Shrieks'}
+
+print(myDict)
+
+#We can use the keys to get the value
+
+print(myDict["Dog"])
+
+#Retrieve items of dictionary
+print(myDict.items())
+
+#We can also use the len() function
+print(len(myDict))
